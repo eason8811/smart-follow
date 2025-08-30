9814a215a6b162be20dbc63b39a71bee9758829b
Eason <3220563569@qq.com>
2025-08-30 19:13:28 +0800

    初始化项目

---
+++ src/main/java/xin/eason/smartfollow/SmartFollowApplication.java
@@ -0,0 +1,13 @@
+package xin.eason.smartfollow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFollowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFollowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xin/eason/smartfollow/SmartFollowApplicationTests.java
@@ -0,0 +1,13 @@
+package xin.eason.smartfollow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFollowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
